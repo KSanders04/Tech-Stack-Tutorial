8e006adb3002cfde53c126c6e0af432bc47014e4
KSanders04 <kasanders2004@gmail.com>
2025-10-15 20:42:16 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello Capstone</Text>
     </View>
   );
 }
@@ -17,4 +15,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    fontWeight: 600,
+  },
 });
+
